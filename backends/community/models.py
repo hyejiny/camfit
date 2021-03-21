670f86a7ff2517e4c5c6adfa43e51bd6d17bc92c
multicampus <op032@naver.com>
2021-03-21 15:39:35 +0900

    create article model

--- backends/community/models.py
+++ backends/community/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    hit = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    purpose = models.IntegerField()
