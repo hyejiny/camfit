501a63f8e3a45a2f6c49300ccfcb5c39aba03a1a
multicampus <op032@naver.com>
2021-03-21 16:37:07 +0900

    create community serializer

--- backends/community/models.py
+++ backends/community/models.py
@@ -9,4 +9,12 @@ class Article(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    purpose = models.IntegerField()
+    category = models.IntegerField()
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    content = models.CharField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
