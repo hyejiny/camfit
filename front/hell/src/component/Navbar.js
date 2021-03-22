7633060c51e3f4ba806602074be05a30e68864a8
이대헌 <eogjs4818@naver.com>
2021-03-23 03:56:35 +0900

    0323 front start-project

---
+++ front/hell/src/component/Navbar.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+class NavBar extends Component {
+    render() {
+        return (
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                <Nav.Link href="/info">정보게시판</Nav.Link>
+                <Nav.Link href="/article">자유게시판</Nav.Link>
+                <Nav.Link href="exercise">ai운동하기</Nav.Link>
+                <Nav.Link href="moneyclass">유료강의</Nav.Link>
+                <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                </NavDropdown>
+                </Nav>
+                <Nav>
+                <Nav.Link href="#deets">More deets</Nav.Link>
+                <Nav.Link eventKey={2} href="#memes">
+                    Dank memes
+                </Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+            </Navbar>
+        )
+    }
+}
+
+export default NavBar
---
+++ front/hell/src/pages/Article.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './page.css';
+
+class Article extends Component {
+    render() {
+        return (
+            <div class="Article">자유게시판</div>
+        )
+    }
+}
+
+export default Article
