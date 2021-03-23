1fc9c5d2b1306fbb1dfe89f41500740db4ebabc5
multicampus <op032@naver.com>
2021-03-23 18:08:04 +0900

    0323 create infoboard model

--- backends/infoboards/models.py
+++ backends/infoboards/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class InfoArticle(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    image = models.ImageField(blank=True, upload_to='%Y%m%d')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.pk} : {self.title}'
