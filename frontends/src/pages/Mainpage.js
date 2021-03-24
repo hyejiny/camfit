d1c41dee8720d5ab7f15f27e42d6b3a885afbbc0
Gyeong-Yeon <lky4156@naver.com>
2021-03-25 01:59:56 +0900

    0325 Front modified Mainpage to add Service Introduction

--- frontends/src/pages/Mainpage.js
+++ frontends/src/pages/Mainpage.js
@@ -5,9 +5,9 @@ class Mainpage extends Component {
     render() {
         return (
             <div className="Mainpage">
-                {/* <img src="@/images/CamFit.jpg" alt=""/>
+                {/* <img src="@/images/CamFit.jpg" alt=""/> */}
                 <h2>홈 피트니스(Fit)를 하는 동안, 웹 카메라(Cam)를 통해 촬영하면</h2>
-                <h2>AI가 사용자의 자세를 음성으로 트레이닝해주는 서비스</h2> */}
+                <h2>AI가 사용자의 자세를 음성으로 트레이닝해주는 서비스</h2>
             </div>
         )
     }
