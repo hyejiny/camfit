121a5a1b26598ad3bffc22b18baa69262f262dae
QT-HH <hyunheal@naver.com>
2021-03-24 10:48:48 +0900

    0324 Back Fix selftrain urls.py

--- backends/selftrains/urls.py
+++ backends/selftrains/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'selftrains'
 urlpatterns = [
     path('', views.train_list_create, name= 'train_list_create'),
-    path('<int:selftrain_pk>/detail/', views.selftrain_detail, name='selftrain_detail'),
-    path('<int:selftrain_pk>/', views.selftrain_update_delete, name='selftrain_update_delete'),
+    path('<int:selftrain_pk>/detail/', views.train_detail, name='train_detail'),
+    path('<int:selftrain_pk>/', views.train_update_delete, name='train_update_delete'),
 ] 
