20950cc4c3534946bfc10de9a532ca70c7772e44
Ubuntu <ubuntu@ip-172-26-0-35.ap-northeast-2.compute.internal>
2021-03-24 15:46:45 +0000

    0325 Back fordocker

---
+++ docker-django/backends/infoboards/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import get_object_or_404
+
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+
+from .serializers import InfoArticleDetailSerializer, InfoArticleSerializer
+from .models import InfoArticle
+
+
+@api_view(['GET','POST'])
+@authentication_classes([JSONWebTokenAuthentication])
+@permission_classes([IsAuthenticated])
+def infoarticle_list_create(request):
+    if request.method == 'GET':
+        infoarticles = InfoArticle.objects.order_by('-pk')
+        serializer = InfoArticleSerializer(infoarticles, many =True)
+        return Response(serializer.data)
+    else:
+        serializer = InfoArticleSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(user= request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET'])
+@authentication_classes([JSONWebTokenAuthentication])
+@permission_classes([IsAuthenticated])
+def infoarticle_detail(request, infoarticle_pk):
+    infoarticle = get_object_or_404(InfoArticle, pk=infoarticle_pk)
+    serializer = InfoArticleDetailSerializer(infoarticle)
+    return Response(serializer.data)
+
+
+@api_view(['PUT','DELETE'])
+@authentication_classes([JSONWebTokenAuthentication])
+@permission_classes([IsAuthenticated])
+def infoarticle_update_delete(request,infoarticle_pk):
+    infoarticle = get_object_or_404(InfoArticle, pk=infoarticle_pk)
+
+    if request.method == 'PUT':
+        serializer = InfoArticleSerializer(infoarticle, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(user= request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+    else:
+        infoarticle.delete()
+        return Response({'id': infoarticle_pk}, status=status.HTTP_204_NO_CONTENT)
