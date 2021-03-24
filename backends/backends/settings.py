57d9710e51de91575c7d2f24666d43500e0b9819
QT-HH <hyunheal@naver.com>
2021-03-24 10:25:00 +0900

    0324 Back Testing Allowed_Hosts

--- backends/backends/settings.py
+++ backends/backends/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '=a)v)@r3je(z6fidzc%i1r+h&#+x2wu3@^78nrx95#bs^chd7y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*','70.12.130.122']
 
