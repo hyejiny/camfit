43b8121f6b52954571b1f72781bfab2727a96f8f
multicampus <op032@naver.com>
2021-03-24 22:18:19 +0900

    0324 create models and serializers

--- backends/schedules/models.py
+++ backends/schedules/models.py
@@ -2,11 +2,7 @@ from django.db import models
 from django.conf import settings
 
 # Create your models here.
-class Schedule(models.Model):
-    date = models.models.DateField(auto_now_add=True)
-
 
 class Grass(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='grasses')
-    Schedule = models.ForeignKey(Schedule, on_delete=models.CASCADE, related_name='grasses')
-    
+    user = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='Grass', blank=True)
+    date = models.models.DateField(auto_now_add=True)
