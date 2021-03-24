f65c8cfee2eb695dae14640de6a1f6daf34c8062
Gyeong-Yeon <lky4156@naver.com>
2021-03-25 01:57:38 +0900

    0325 Front modified Navbar pages name

--- frontends/src/pages/Mainpage.js
+++ frontends/src/pages/Mainpage.js
@@ -4,7 +4,11 @@ import './page.css';
 class Mainpage extends Component {
     render() {
         return (
-            <div className="Mainpage">메인페이지</div>
+            <div className="Mainpage">
+                {/* <img src="@/images/CamFit.jpg" alt=""/>
+                <h2>홈 피트니스(Fit)를 하는 동안, 웹 카메라(Cam)를 통해 촬영하면</h2>
+                <h2>AI가 사용자의 자세를 음성으로 트레이닝해주는 서비스</h2> */}
+            </div>
         )
     }
 }
