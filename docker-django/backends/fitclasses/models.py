63119d2e92e81e9040ff0efbafa35c3535ce65db
QT-HH <hyunheal@naver.com>
2021-03-26 11:24:10 +0900

    0326 Back Create models.py at fitclasses app

--- docker-django/backends/fitclasses/models.py
+++ docker-django/backends/fitclasses/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Fitclass(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    image = models.ImageField(blank=True)
+    start_day = models.CharField(max_length=50)
+    end_day = models.CharField(max_length=50)
+    user_limit = models.IntegerField()
+    price = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
