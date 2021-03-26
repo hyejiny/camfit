0204ec80ebc3cf718142b1bad41a8ca5f2b7cb15
QT-HH <hyunheal@naver.com>
2021-03-26 12:18:53 +0900

    0326 Back Modify serializers.py at fitclasses

--- docker-django/backends/fitclasses/serializers.py
+++ docker-django/backends/fitclasses/serializers.py
@@ -1,8 +1,19 @@
 from rest_framework import serializers
 from .models import Fitclass
 
+from django.contrib.auth import get_user_model
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = get_user_model()
+        fields=('username')
+
 class FitclassListSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    guests = UserSerializer(required=False, many=True)
 
     class Meta:
         model = Fitclass
-        fields = ('title', 'image', 'start_day', 'end_day', )
+        fields = '__all__'
+        read_only_fields = {'user'}
