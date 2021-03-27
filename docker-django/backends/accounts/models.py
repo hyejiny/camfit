2bbbd26f6b14ac209ed0f4cf8cba116f26505715
QT-HH <hyunheal@naver.com>
2021-03-27 14:25:12 +0900

    0327 Back Create Alert Model at accounts app

--- docker-django/backends/accounts/models.py
+++ docker-django/backends/accounts/models.py
@@ -8,3 +8,9 @@ class User(AbstractUser):
     )
 
     category = models.IntegerField(choices=user_category, default=1)
+
+class Alert(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='alerts')
+    checked = models.BooleanField(default=False)
+    content = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
