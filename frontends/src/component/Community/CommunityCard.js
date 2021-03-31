6c28464126a5d7eea244cac05b83aeef4fe59234
Gyeong-Yeon <lky4156@naver.com>
2021-03-31 17:14:24 +0900

    0331 front modified CommunityCard

--- frontends/src/component/Community/CommunityCard.js
+++ frontends/src/component/Community/CommunityCard.js
@@ -1,22 +1,84 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import List from '../component/list';
+import UploadPost from '../component/uploadPost';
+import { boardDelete, boardSave, boardSelected } from '../reducer/board';
 
-function mapStateToProps(state) {
-    return {
 
-    };
-}
+function CommunityCard() {
 
-class CommunityCard extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
+    const [post, setPost] = useState({
+        id: '',
+        title: '',
+        content: ''
+    });
+
+    const dispatch = useDispatch();
+
+    const onDelete = (postId) => dispatch(boardDelete(postId));
+    const onSave = (dataToSubmit) => dispatch(boardSave(dataToSubmit));
+
+    const {selected} = useSelector(state => state.board);
+
+    const postClickHandler = (postId) => 
+    {
+        dispatch(boardSelected(postId));
+        if(JSON.stringify(selected) !== '{}') {
+            setPost(selected);
+        }
     }
+
+    const changeInput = (e) => {
+        setPost({
+            ...post,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const resetForm = () => {
+        setPost({
+            id: '', title: '', content: ''
+        })
+    }
+
+    const {boards} = useSelector(state => state.board);
+
+    return (
+        <div>
+            <div>
+                <table>
+                    <tbody>
+                        <tr align="center">
+                            <td width="50">번호</td>
+                            <td width="100">제목</td>
+                            <td width="200">내용</td>
+                        </tr>
+                        {
+                            boards.map(post =>
+                            (
+                                <List 
+                                    key={post.id}
+                                    id={post.id}
+                                    title={post.title}
+                                    content={post.content}
+                                    onDelete={onDelete}
+                                    postClickHandler={postClickHandler}
+                                />
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </div>
+            <div>
+                <UploadPost 
+                    onSave={onSave} 
+                    changeInput={changeInput} 
+                    post={post}
+                    resetForm={resetForm} 
+                />
+            </div>
+        </div>
+    );
 }
 
-export default connect(
-    mapStateToProps,
-)(CommunityCard);
+export default CommunityCard;
--- frontends/src/index.js
+++ frontends/src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
