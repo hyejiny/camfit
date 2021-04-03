ae211b8bdbff2f64832a65c33dc16dffcd9fe980
DESKTOP-KVCQHCD\multicampus <op032@naver.com>
2021-04-03 20:27:35 +0900

    0403 fileupload plus image

--- frontends/src/component/utils/FileUpload.js
+++ frontends/src/component/utils/FileUpload.js
@@ -3,29 +3,55 @@ import { useDispatch } from "react-redux";
 import Dropzone from 'react-dropzone'
 import { PlusOutlined } from '@ant-design/icons';
 import { imageshow, imageget } from "../../_actions/index";
-import axios from 'axios';
+import { API_BASE_URL } from "../../constants";
 
 
 function FileUpload() {
 
-    const [image, setimage] = useState({})
+    const [image, setimage] = useState()
     const dispatch = useDispatch()
     const fileChangedHandler = (event) => {
-        const file = event.target.files[0]
+        const file = event.target.files[0].uploadedFile
+        console.log(file);
         setimage({ selectedFile: event.target.files[0] })
+        console.log('resailt',event.target.result)
         // dispatch(imageshow(image))
     }
       
     const uploadHandler = () => { 
-        console.log(image) 
-        dispatch(imageshow(image))
-        // dispatch(imageget())
-        // .then((res) => {
-        //     const image_list = res.payload
-        //     console.log(image_list);
-        //     setimage(image_list)
-        // })
-        // const formData = new FormData()
+
+        
+        // console.log('uploadHandler', image['selectedFile']) 
+        const form = new FormData();
+        form.append('file', image['selectedFile'])
+        form.append('enctype', 'multipart/form-data')
+        for (let value of form.values()) {
+            console.log('for ,' ,value);
+        }
+        dispatch(imageshow(form))
+        dispatch(imageget())
+        .then((res) => {
+            const image_list = res.payload
+
+            console.log(image_list);
+            // setimage(image_list[0])
+            var binaryData = [];
+            binaryData.push(image_list[0]);
+            const imageurl = URL.createObjectURL(new Blob(binaryData))
+            console.log(imageurl);
+            var dat = imageurl;
+            var st = String(dat);
+            var shot_d = st.substring(5);
+            console.log(shot_d);
+            setimage(shot_d)
+            
+            
+        })
+        
+        
+        
+
+        
     }
 
 
@@ -34,6 +60,12 @@ function FileUpload() {
         <div>
             <input type="file" onChange={fileChangedHandler}/>
             <button onClick={uploadHandler}>Upload!</button>
+            <div>
+                <img 
+                src={image} />
+            </div>
+            
+                <hr/>
         </div>
     )
 }
