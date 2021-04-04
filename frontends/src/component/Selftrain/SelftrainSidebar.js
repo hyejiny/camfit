ddb5d36dc6ed4f9b91a2ab322dc02999327f27b1
혜진 <gpwls2127@gmail.com>
2021-04-05 04:23:43 +0900

    0405 Front update 트레이닝 페이지

--- frontends/src/component/Selftrain/SelftrainSidebar.js
+++ frontends/src/component/Selftrain/SelftrainSidebar.js
@@ -3,9 +3,10 @@ import "../component.css";
 import { useDispatch } from "react-redux";
 import { selftrainlist } from "../../_actions/index";
 import { ListGroup, Card, Button, Row, Col } from "react-bootstrap";
-import { Drawer } from "antd";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import { Image, Reveal } from "semantic-ui-react";
 
 function SelftrainSidebar(props) {
   // const category_list = useState([
@@ -42,18 +43,73 @@ function SelftrainSidebar(props) {
 
   return (
     <div className="site-content">
-      <Row xs={1} md={2} lg={4}>
+      <Row xs={1} md={4}>
         <Col>
-          <Card
+          <Reveal animated="rotate">
+            <Reveal.Content visible>
+              어깨
+              <Image circular size="medium" src="https://ifh.cc/g/b70G9w.jpg" />
+            </Reveal.Content>
+            <Reveal.Content hidden>
+              <Button onClick={arm} className="mt-3">시작하기</Button>
+            </Reveal.Content>
+          </Reveal>
+        </Col>
+
+        <Col>
+        <Reveal animated="rotate">
+            <Reveal.Content visible>
+              코어
+              <Image circular size="medium" src="https://ifh.cc/g/uKNRvB.jpg" />
+            </Reveal.Content>
+            <Reveal.Content hidden>
+              <Button onClick={arm} className="mt-3">시작하기</Button>
+            </Reveal.Content>
+            </Reveal>
+        </Col>
+    
+        <Col>
+        <Reveal animated="rotate">
+        <Reveal.Content visible>
+              팔
+              <Image circular size="medium" src="https://ifh.cc/g/z85Vm7.jpg" />
+            </Reveal.Content>
+            <Reveal.Content hidden>
+              <Button onClick={arm} className="mt-3">시작하기</Button>
+            </Reveal.Content>
+            </Reveal>
+        </Col>
+
+        <Col>
+        <Reveal animated="rotate">
+        <Reveal.Content visible>
+              하체
+              <Image circular size="medium" src="https://ifh.cc/g/Q7zSHG.jpg" />
+            </Reveal.Content>
+            <Reveal.Content hidden>
+              <Button onClick={arm} className="mt-3">시작하기</Button>
+            </Reveal.Content>
+            </Reveal>
+        </Col>
+      </Row>
+
+      {/* <Row xs={1} md={2} lg={4}>
+        <Col>
+          <List
             text="black"
             style={{ width: "20rem", height: "18rem" }}
             className="mb-2 bgc"
           >
-            <Card.Body>
+            <List.Item>
+            <List.Item.Meta
+          avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+          title={<div>어깨</div>}
+          description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+        />
               <Card.Title> 어깨 </Card.Title>
               <Button onClick={shoulder}>시작하기</Button>
-            </Card.Body>
-          </Card>
+            </List.Item>
+          </List>
         </Col>
 
         <Col>
@@ -94,7 +150,7 @@ function SelftrainSidebar(props) {
             </Card.Body>
           </Card>
         </Col>
-      </Row>
+      </Row> */}
     </div>
   );
 }
